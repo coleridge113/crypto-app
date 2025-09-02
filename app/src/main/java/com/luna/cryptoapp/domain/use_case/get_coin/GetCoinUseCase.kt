e78b154e7f515db5654b8f2155497c51ee90a32d
Jose Luna <josemanuelmluna@gmail.com>
2025-09-02 15:03:11 +0800

    updated use cases to use dispatchers IO

--- app/src/main/java/com/luna/cryptoapp/domain/use_case/get_coin/GetCoinUseCase.kt
+++ app/src/main/java/com/luna/cryptoapp/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -1,11 +1,12 @@
 package com.luna.cryptoapp.domain.use_case.get_coin
 
 import com.luna.cryptoapp.common.Resource
-import com.luna.cryptoapp.domain.model.Coin
 import com.luna.cryptoapp.domain.model.CoinDetail
 import com.luna.cryptoapp.domain.repository.CoinRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
@@ -13,16 +14,18 @@ import javax.inject.Inject
 class GetCoinUseCase @Inject constructor(
     private val repository: CoinRepository
 ) {
-    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> {
-        return flow {
-            try {
-                emit(Resource.Loading())
-                val coin = repository.getCoinById(coinId)
-                emit(Resource.Success(coin))
-            } catch (e: HttpException) {
-                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
-            } catch (e: IOException) {
-                emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+    suspend operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> {
+        return withContext(Dispatchers.IO) {
+            flow {
+                try {
+                    emit(Resource.Loading())
+                    val coin = repository.getCoinById(coinId)
+                    emit(Resource.Success(coin))
+                } catch (e: HttpException) {
+                    emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+                } catch (e: IOException) {
+                    emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+                }
             }
         }
     }
--- app/src/main/java/com/luna/cryptoapp/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/luna/cryptoapp/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -3,8 +3,10 @@ package com.luna.cryptoapp.domain.use_case.get_coins
 import com.luna.cryptoapp.common.Resource
 import com.luna.cryptoapp.domain.model.Coin
 import com.luna.cryptoapp.domain.repository.CoinRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
@@ -12,15 +14,19 @@ import javax.inject.Inject
 class GetCoinsUseCase @Inject constructor(
     private val repository: CoinRepository
 ) {
-    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
-            try {
-                emit(Resource.Loading())
-                val coins = repository.getCoins()
-                emit(Resource.Success(coins))
-            } catch (e: HttpException) {
-                emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
-            } catch (e: IOException) {
-                emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+    suspend operator fun invoke(): Flow<Resource<List<Coin>>> {
+        return withContext(Dispatchers.IO) {
+            flow {
+                try {
+                    emit(Resource.Loading())
+                    val coins = repository.getCoins()
+                    emit(Resource.Success(coins))
+                } catch (e: HttpException) {
+                    emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+                } catch (e: IOException) {
+                    emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+                }
             }
         }
+    }
 }
